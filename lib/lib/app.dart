16f87c8800bcc960dcafe8a122b2c0cc072c04d7
Gj1337 <valentyn.hlyvich@gmail.com>
2024-11-20 19:33:25 +0200

    Deploy action

--- lib/lib/app.dart
+++ lib/lib/app.dart
@@ -2,15 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:solid_software_test_task/lib/presentation/main_screen/main_screen.dart';
 
 /// App class that is start point for all application
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   // ignore: public_member_api_docs
   const App({super.key});
 
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
